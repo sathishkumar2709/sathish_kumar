bf8dddb6b8258aba5b4c6e1d356b19be8c4b9087
sathishkumar2709 <134679317+sathishkumar2709@users.noreply.github.com>
2024-01-23 00:32:54 -0800

    Add files via upload

---
+++ KitchenApplication/src/test/java/ProjectKitchen/Kitchen/KitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectKitchen.Kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
